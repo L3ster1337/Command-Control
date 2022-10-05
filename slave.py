7096ad44ff068fb9018034f794a3b2843e63483e
L3ster in 1337 <44538226+L3ster1337@users.noreply.github.com>
2022-10-05 19:45:59 -0400

    Update slave.py

--- slave.py
+++ slave.py
@@ -5,14 +5,16 @@ import os
 import subprocess 
 from subprocess import PIPE, Popen
 
-cmd = sys.argv[2]
+user = sys.argv[2]
+password = sys.argv[3]
+cmd = sys.argv[4]
 
 
-if(len(sys.argv) < 2):
-	print("Usage: python3 slave.py 127.0.0. 'uname -a' -> deve-se omitir o valor do ultimo bloco")
+if(len(sys.argv) <= 4):
+	print("Usage: python3 slave.py 13.50.18. bandit0 bandit0 'id' -> deve-se omitir o valor do ultimo bloco do IP\n python3 slave.py <IP-SEM-FINAL> <User> <Pass> '<Comando>'") #primeiro bandit0 eh o user, o segundo a senha
 	sys.exit()
 
-for x in range(242,245):
+for x in range(242,245): #range proposital para testar o .243, IP do Bandit0, assim como verificar o funcionamento do timeout 
 
 	var1 = sys.argv[1]
 	var2 = str(x)
@@ -22,8 +24,8 @@ for x in range(242,245):
 	try:
 
 		print('[Info] Attempt on host: ' + host + '\n')
-		connect = 'bandit0@' + host
-		stream = subprocess.run(['sshpass', '-p', 'bandit0', 'ssh', connect, '-p','2220', cmd], timeout=7)
+		connect = user + '@' + host
+		stream = subprocess.run(['sshpass', '-p', password, 'ssh', connect, '-p','2220', cmd], timeout=7) # Timeout setado para 7 segundos
 		
 
 	except subprocess.TimeoutExpired as e:
